db29ad80d6529bfcf3a51265a252e498f14e4f9a
Felix1kk <102288435+Felix1kk@users.noreply.github.com>
2024-12-23 13:51:46 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,6 +3,11 @@ import streamlit as st
 from streamlit_cookies_manager import EncryptedCookieManager
 from firebase_config import logout_user
 
+st.set_page_config(
+    page_title="MedMRI AI",
+    page_icon="☢️"
+)
+
 # Custom CSS to hide Streamlit icon, GitHub, and Fork icons
 hide_streamlit_style = """
 <style>
@@ -17,14 +22,13 @@ footer .stMetrics {display: none;}  /* Hide the Streamlit logo */
 # Inject custom CSS
 st.markdown(hide_streamlit_style, unsafe_allow_html=True)
 
-
 firebase_api_key = st.secrets["firebase"]["api_key"]
 firebase_password = st.secrets["firebase"]["password"]
 
 # Initialize cookies
 cookies = EncryptedCookieManager(
-    prefix=firebase_api_key,
-    password=firebase_password 
+    prefix=firebase_api_key, #brain_tumor_app
+    password=firebase_password #ezechinyere
 )
 if not cookies.ready():
     st.stop()
@@ -35,9 +39,12 @@ current_page = cookies.get("current_page", "Login")
 user_id = cookies.get("user_id")
 
 # Sync session state with cookies
-st.session_state["logged_in"] = st.session_state.get("logged_in", logged_in)
-st.session_state["page"] = st.session_state.get("page", current_page)
-st.session_state["user_id"] = st.session_state.get("user_id", user_id)
+if "logged_in" not in st.session_state:
+    st.session_state["logged_in"] = logged_in
+if "page" not in st.session_state:
+    st.session_state["page"] = current_page
+if "user_id" not in st.session_state:
+    st.session_state["user_id"] = user_id
 
 # Page routing
 if st.session_state["page"] == "Login":
@@ -66,19 +73,14 @@ with st.sidebar:
             st.rerun()
     else:
         if st.button("Logout"):
-            # Ensure user logout logic is consistent
             if st.session_state.get("user_id"):
-                logout_user(st.session_state["user_id"])  # Update the logout time (if applicable)
+                logout_user(st.session_state["user_id"])  # Update the logout time
             cookies["logged_in"] = str(False)
             cookies["current_page"] = "Login"
             cookies["user_id"] = ""
             cookies.save()
-
-            # Clear session state
-            for key in list(st.session_state.keys()):
-                del st.session_state[key]
-
-            # Redirect to login page
+            st.session_state.clear()
             st.rerun()
+
 
 
